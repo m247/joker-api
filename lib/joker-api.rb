72babf8ee5d594fe2b8fa6410da588cff3781bbd
Geoff Garside <geoff@geoffgarside.co.uk>
2012-08-30 09:14:32 +0100

    Add initial JokerAPI::Client class.

--- lib/joker-api.rb
+++ lib/joker-api.rb
@@ -1,4 +1,5 @@
 require File.expand_path('../joker-api/version', __FILE__)
 
 module JokerAPI
+  autoload :Client,     File.expand_path('../joker-api/client.rb', __FILE__)
 end
---
+++ lib/joker-api/client.rb
@@ -0,0 +1,18 @@
+require 'httparty'
+
+module JokerAPI
+  class Client
+    include HTTParty
+
+    def initialize(username, password, host = 'dmapi.joker.com', options = {})
+      @username, @password = username, password
+      @default_options = options.merge(:base_uri => HTTParty.normalize_base_uri("https://#{host}"))
+
+      @auth_sid = nil
+    end
+
+    def inspect
+      "#<#{self.class.name} #{@default_options[:base_uri]} (#{@username})#{@auth_sid && " logged in"}>"
+    end
+  end
+end
