d2773727e661c22e8a51af77ac0259d4bff1665a
Geoff Garside <geoff@geoffgarside.co.uk>
2012-11-26 14:02:10 +0000

    Implement domain-renew operation.

--- lib/joker-api/operations/domain_renew.rb
+++ lib/joker-api/operations/domain_renew.rb
@@ -4,7 +4,14 @@ module JokerAPI
       # @param [String] domain Domain name to renew
       # @param [Integer] period Number of months to renew the domain for
       def domain_renew(domain, period)
+        raise ArgumentError, "period must be integer" unless period.kind_of?(Fixnum)
 
+        response = perform_request("domain-renew", {
+          :domain => domain, :period => period.to_s })
+
+        # need to know what the response contents actually are
+        @domain_renew_response = response # for inspection purposes
+        response.success?
       end
     end
   end
