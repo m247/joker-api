9f09ed320103ac14512e0d422af040c374f6198b
Geoff Garside <geoff@geoffgarside.co.uk>
2012-11-28 16:00:24 +0000

    Add the DomainOwnerChange operation.

--- lib/joker-api/operations/domain_owner_change.rb
+++ lib/joker-api/operations/domain_owner_change.rb
@@ -5,7 +5,6 @@ module JokerAPI
     module DomainOwnerChange
       # @param [String] domain Domain name to change the owner of
       # @param [Hash] fields New owner fields
-      # @option fields [String] :tld TLD where this contact is to be used.
       # @option fields [String] :name Full name, if blank :fname and :lname will be used
       # @option fields [String] :fname First name, required if :name is blank
       # @option fields [String] :lname Last name, required if :name is blank
@@ -22,7 +21,23 @@ module JokerAPI
       # @option fields [String] :extension Telephone extension. Optional field.
       # @option fields [String] :fax Fax number. Optional field.
       def domain_owner_change(domain, fields = {})
+        raise ArgumentError, ":organization required if :individual is false" if fields[:individual] && fields[:individual] == false && fields[:organization].blank?
+        raise ArgumentError, ":name or :fname and :lname are required" if fields[:name].blank? && fields[:fname].blank? && fields[:lname].blank?
 
+        [:email, :address, :city, :postal_code, :country, :phone].each do |field|
+          raise ArgumentError, "field :#{field} is required" if fields[field].blank?
+        end
+
+        fields['individual'] = fields.delete(:individual) ? 'Y' : 'N'
+        fields['postal-code'] = fields.delete(:postal_code)
+
+        Array(fields.delete(:address)).each_with_index do |addr, idx|
+          break if idx > 2
+          fields["address-#{idx + 1}"] = addr
+        end
+
+        response = perform_request('domain-owner-change', fields.merge(:domain => domain))
+        response.success?
       end
     end
   end
