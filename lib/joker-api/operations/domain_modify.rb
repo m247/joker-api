37386563b4978e4d716f7bdbb687f9b0620cac6d
Geoff Garside <geoff@geoffgarside.co.uk>
2012-11-29 11:40:59 +0000

    Finish implementation of DomainModify operation.

--- lib/joker-api/operations/domain_modify.rb
+++ lib/joker-api/operations/domain_modify.rb
@@ -4,7 +4,6 @@ module JokerAPI
   module Operations
     module DomainModify
       VALID_OPTIONS = [:billing_c, :admin_c, :tech_c, :ns_list, :dnssec]
-      SUPPORTED_TLDs = /\.(com|net|org|tv|cc|de)$/
 
       # @param [String] domain Domain name to modify
       # @param [Hash] options
@@ -12,10 +11,32 @@ module JokerAPI
       # @option options [String] :admin_c New Admin contact handle
       # @option options [String] :tech_c New Technical contact handle
       # @option options [Array<String>] :ns_list New list of nameservers
-      # @option options [Array,Boolean] :dnssec Set to false to remove DNSSEC.
+      # @option options [Array<String>,Boolean] :dnssec Set to false to remove DNSSEC.
+      #                 The string values should be tag:alg:digest-type:digest for com/net/org/tv/cc
+      #                 For .de domains the values should be protocol:alg:flags:pubkey-base64
       def domain_modify(domain, options = {})
-        raise ArgumentError, "modification not supported for this domain" unless domain =~ SUPPORTED_TLDs
         options.assert_valid_keys(VALID_OPTIONS)
+        return true if options.empty?
+
+        options["tech-c"] = options.delete(:tech_c)
+        options["admin-c"] = options.delete(:admin_c)
+        options["billing-c"] = options.delete(:billing_c)
+
+        options["ns-list"] = options.delete(:ns_list).join(":")
+
+        dnssec = options.delete(:dnssec)
+        options["dnssec"] = "0" if dnssec == false
+
+        if dnssec
+          options["dnssec"] = "1"
+          dnssec.each_with_index do |obj, idx|
+            break if idx > 5 # not allowed more than 6 entries
+            options["ds-#{idx+1}"] = obj
+          end
+        end
+
+        response = perform_request('domain-modify', options.merge(:domain => domain))
+        return response.success?
       end
     end
   end
