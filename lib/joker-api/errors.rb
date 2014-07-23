be33ddf8a274c221c1c8e137507e9f67c6e4705e
Geoff Garside <geoff@geoffgarside.co.uk>
2014-07-23 10:37:56 +0100

    Include response in JokerAPI::IncompleteRequest
    Allows catching code to attempt another result_retrieve external to the
    gem code.

--- lib/joker-api/errors.rb
+++ lib/joker-api/errors.rb
@@ -1,5 +1,11 @@
 module JokerAPI
   class AuthorisationError < RuntimeError; end
   class ObjectNotFound < RuntimeError; end
-  class IncompleteRequest < RuntimeError; end
+  class IncompleteRequest < RuntimeError
+    attr_reader :response
+    def initialize(message, response = nil)
+      super message
+      @response = response
+    end
+  end
 end
